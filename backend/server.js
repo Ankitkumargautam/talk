9ebff463b986e485e523b0c58b52036704820de3
Ankitkumargautam <ankitogn4@gmail.com>
2024-01-10 22:59:51 +0530

    Initial file for backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { chats } = require('./data/data');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Application started');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
+  // console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
